936a775c099d869e87561923dac6de89a45b3144
pulkitbest <pulkitgupta38@gmail.com>
2023-12-18 15:24:17 +0530

    added __dirname

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,9 @@ newMonth()
 
 app.use('/api/users', userRoutes)
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '/frontend/build')))
 
