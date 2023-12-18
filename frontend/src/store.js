65150956911d47cf079355e54407b87defe6db31
pulkitbest <pulkitgupta38@gmail.com>
2023-12-18 15:11:59 +0530

    redux-devtools removed

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { thunk } from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+
 import {
     userLoginReducer, 
     userRegisterReducer, 
@@ -22,6 +22,6 @@ const initialState = {
 }
 
 const middleware = [thunk]
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(reducer, initialState, applyMiddleware(...middleware))
 
 export default store
